b83585585457b21baf6064424dc8d0b655396021
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-03-21 17:47:39 +0700

    POST/GET

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
-app = FastAPI() #tạo app đây là trái tim server 
+app = FastAPI() #tạo app đây là trái tim server
 
+#database tạm thời chỉ là list trong ram
+tasks = []
 @app.get("/") # báo cho FastAPI biết: "khi ai gọi GET tới / thì chạy hàm bên dưới
 def hello():
     return {"massage": "Hello World!"}
+
+@app.get("/tasks") 
+def getTask():
+    return tasks
+#POST /task nhận task mới từ body sang list 
+@app.post("/task")  
+def create_task(task:dict):
+    tasks.append(task)
+    return {"message": "Thêm task thành công","task": task}
 
